4a681884b2be5c96ccdb311c70ac48e4be5f2eec
Robertotorre96 <torreroberto1996@gmail.com>
2026-04-30 23:01:23 -0300

    delete admin hasData

--- ApiCine/Features/Usuario/UsuarioConfig.cs
+++ ApiCine/Features/Usuario/UsuarioConfig.cs
@@ -17,14 +17,7 @@ namespace ApiCine.Features.Usuario {
             builder.Property(u => u.Email).IsRequired().HasMaxLength(100);
             builder.Property(u => u.Role).IsRequired().HasConversion<string>();
 
-            builder.HasData(new UsuarioEntity {
-                Id = 1,
-                Username = "admin",
-                Nombre = "Admin General",
-                Email = "admin@apicine.com",
-                Password = BCrypt.Net.BCrypt.HashPassword("Admin123*"),
-                Role = ERole.Admin
-            });
+          
         }
 
 
